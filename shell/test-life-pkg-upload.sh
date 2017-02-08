193486a0d4e5adf6ade6dcda927ecc155fc516c7
nikytwo <nikytwo@gmail.com>
2017-02-08 16:18:02 +0800

    fix: 测试版从 stage 分支获取

--- shell/test-life-pkg-upload.sh
+++ shell/test-life-pkg-upload.sh
@@ -1,7 +1,7 @@
 
 # 检出最新的 trunk 分支代码
 cd skg-headline-root/
-svn sw --username life_package --password !Life714. http://10.10.99.114:8081/svn/skg-headline/trunk .
+svn sw --username life_package --password !Life714. http://10.10.99.114:8081/svn/skg-headline/branches/v2.x_stage .
 
 # 打包
 mvn clean package -Dmaven.test.skip=true
